f78a383d80f8aa10e7c23d4d922ae36defcbc4d7
Daniel Johnston <djjoshnton98@hotmail.com>
2024-11-24 02:46:26 +0000

    add users app

---
+++ users/models.py
@@ -0,0 +1,8 @@
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+class CustomUser(AbstractUser):
+    bio = models.TextField(blank=True, null=True)
+
+    def __str__(self):
+        return self.username

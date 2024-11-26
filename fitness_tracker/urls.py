749d15c2db8bd3df383e86eea6442dba837cc6ef
Daniel Johnston <djjoshnton98@hotmail.com>
2024-11-26 00:25:19 +0000

    Add home page to main views, adjust main urlpatterns

--- fitness_tracker/urls.py
+++ fitness_tracker/urls.py
@@ -15,16 +15,18 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('allauth.urls')),
-    path('', include('users.urls')),
-    path('', include('goals.urls')),
-    path('', include('workouts.urls')),
-    path('', include('notifications.urls')),
-    path('', include('nutrition.urls')),
-    path('', include('progress.urls')),
-    path('', include('wearables.urls')),
+    path('users/', include('users.urls', namespace='users')),
+    path('goals/', include('goals.urls', namespace='goals')),
+    path('workouts/', include('workouts.urls', namespace='workouts')),
+    path('notifications/', include('notifications.urls', namespace='notifications')),
+    path('nutrition/', include('nutrition.urls', namespace='nutrition')),
+    path('progress/', include('progress.urls', namespace='progress')),
+    path('wearables/', include('wearables.urls', namespace='wearables')),
+    path('', views.home, name='home'),
 ]

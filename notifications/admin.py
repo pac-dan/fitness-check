31fc03596cd34ceedf42a7d2bbc4791b32c7ea65
Daniel Johnston <djjoshnton98@hotmail.com>
2024-12-05 20:36:05 +0000

    change user list to sign up/sign in.. Delete Wearables,notifications apps

--- notifications/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import Notification
-
-@admin.register(Notification)
-class NotificationAdmin(admin.ModelAdmin):
-    list_display = ('title', 'user', 'read', 'created_at', 'updated_at')
-    search_fields = ('title', 'message', 'user__username')
-    list_filter = ('user', 'read', 'created_at', 'updated_at')
--- wearables/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import Wearable
-
-@admin.register(Wearable)
-class WearableAdmin(admin.ModelAdmin):
-    list_display = ('device_name', 'brand', 'model', 'user', 'purchase_date', 'created_at', 'updated_at')
-    search_fields = ('device_name', 'brand', 'model', 'user__username')
-    list_filter = ('brand', 'purchase_date', 'created_at', 'updated_at')

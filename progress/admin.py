7d30f5f292cded3b7c648829531a40564d42e25f
Daniel Johnston <djjoshnton98@hotmail.com>
2024-12-17 23:39:56 +0000

    Remove progress app

--- progress/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import Progress
-
-@admin.register(Progress)
-class ProgressAdmin(admin.ModelAdmin):
-    list_display = ('user', 'date', 'weight', 'body_fat_percentage', 'muscle_mass', 'created_at', 'updated_at')
-    search_fields = ('user__username', 'notes')
-    list_filter = ('user', 'date', 'created_at', 'updated_at')
